8ea2dcdf3727deacf73848055c6aceddee6c7ab2
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-24 21:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Create a student object
+const student = {
+  name: "John Doe"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+
